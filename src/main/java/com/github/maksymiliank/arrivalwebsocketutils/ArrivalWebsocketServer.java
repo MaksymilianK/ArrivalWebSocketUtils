68ea916331c96e6e7e9091721fac73ce89988c73
MaksymilianK <kondziugeniusz@gmail.com>
2021-10-23 18:58:41 +0200

    Init project

---
+++ src/main/java/com/github/maksymiliank/arrivalwebsocketutils/ArrivalWebsocketServer.java
@@ -0,0 +1,35 @@
+package com.github.maksymiliank.arrivalwebsocketutils;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+
+public class ArrivalWebsocketServer extends WebSocketServer {
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+}
